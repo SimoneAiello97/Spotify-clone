773943bed615e769d53f2dcc3900b837d1ff0869
Luca Sernicola <luca.sernicola@gmail.com>
2023-05-16 14:16:23 +0200

    hero card

--- project/script/home.js
+++ project/script/home.js
@@ -1,6 +1,7 @@
 const ALBUM_URL = "https://striveschool-api.herokuapp.com/api/deezer/album/";
 
 const mainCol = document.querySelector("#rowPrincipal");
+const heroSec = document.querySelector(".hero");
 let fetchedAlbums = 0;
 
 const fetchAlbum = async () => {
@@ -41,6 +42,43 @@ const fetchAlbum = async () => {
   }
 };
 
+const getHeroCard = async () => {
+  try {
+    const randomAlbumId = Math.floor(Math.random() * 1000001);
+    const res = await fetch(ALBUM_URL + randomAlbumId);
+    console.log("RES", res);
+    if (res.ok) {
+      const album = await res.json();
+      if (album.id) {
+        let heroCard = `
+        <div class="card">
+          <a href="${album.id}">
+            <div class="row">
+              <div class="col">
+                  <img src="${album.cover_medium}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col">
+                <div class="card-body">
+                  <h5 class="card-title">${album.title}</h5>
+                  <p class="card-text">${album.artist.name}</p>
+                  <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        `;
+        heroSec.innerHTML += heroCard;
+        console.log("RES", album);
+      }
+    } else {
+      throw new Error("Errore nel recupero del prodotto!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const fetchRandomAlbums = async () => {
   while (fetchedAlbums < 6) {
     await fetchAlbum();
@@ -48,3 +86,4 @@ const fetchRandomAlbums = async () => {
 };
 
 fetchRandomAlbums();
+getHeroCard();
