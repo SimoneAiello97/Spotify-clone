c49a8043076251b923b673d9e65313fde1975715
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-05-19 15:42:03 +0200

    final commit

--- project/script/play-pause.js
+++
@@ -1,5 +0,0 @@
-const playButton = document.getElementById('playButton');
-const songTitle = document.getElementById('songTitle');
-playButton.addEventListener('click', () => {
-    songTitle.style.animationPlayState = 'running';
-});
